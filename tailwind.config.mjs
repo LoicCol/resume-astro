935e345f85466b2b05aaddc246feb51a16a36f41
Loic <loic-col@hotmail.fr>
2024-10-19 17:03:03 +0200

    working on menu colors

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,19 +2,6 @@
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    // fontSize: {
-    // 	sm: '0.80rem',
-    // 	base: '1rem',
-    // 	xl: '1.25rem',
-    // 	'2xl': '1.5rem',
-    // 	'3xl': '1.875rem',
-    // 	'4xl': '2.25rem',
-    // 	'5xl': '3rem',
-    // 	'6xl': '4rem',
-    // 	'7xl': '5rem',
-    // 	'8xl': '6rem',
-    // 	'9xl': '7rem'
-    // },
     extend: {
       colors: {
         "nice-white": "#e4e4e0",
@@ -63,6 +50,11 @@ export default {
         lg: "var(--radius)",
         md: "calc(var(--radius) - 2px)",
         sm: "calc(var(--radius) - 4px)",
+      },
+      transitionDuration: {
+        2000: "2000ms",
+        3000: "3000ms",
+        4000: "4000ms",
       },
     },
   },
