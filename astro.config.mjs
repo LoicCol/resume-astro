e77866a1e3e804f1602c330160f02d4187684076
your_name_here <your_name@gopigment.com>
2024-12-28 13:54:21 +0100

    Navigate on scroll

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 
-import vercel from "@astrojs/vercel/static";
+import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
